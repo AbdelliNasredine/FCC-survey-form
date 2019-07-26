461f2665f304180d858b7bc6451a6d5c11fe0f49
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-07-26 12:07:13 +0100

    finished some js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const nextBtn = document.getElementById('next')
 const prevBtn = document.getElementById('previous');
 const lastIndex = questions.length - 1;
 let currentIndex = 0;
+const isFirst = () => currentIndex == 0;
+const isLast = () => currentIndex === lastIndex;
 const isNext = () => questions[currentIndex + 1] != undefined;
 const isPrev = () => questions[currentIndex - 1] != undefined;
 const _switch = (current, next) => {
@@ -10,19 +12,22 @@ const _switch = (current, next) => {
     next.classList.add("active");
 }
 
-nextBtn.addEventListener("click", () => {
-    if(isNext()){
-        _switch(questions[currentIndex] , questions[currentIndex + 1]);
-        currentIndex++;
-    }
-    console.log(currentIndex);
-});
+window.addEventListener('load', () => {
+    console.log("loaded");
+    nextBtn.addEventListener("click", (event) => {
+        if (isNext()) {
+            _switch(questions[currentIndex], questions[currentIndex + 1]);
+            currentIndex++;
+        }
+        console.log(currentIndex);
+    });
 
-prevBtn.addEventListener("click", () => {
-    if(isPrev()){
-        _switch(questions[currentIndex] , questions[currentIndex - 1]);
-        currentIndex--;
-    }
-    console.log(currentIndex);
-});
+    prevBtn.addEventListener("click", () => {
+        if (isPrev()) {
+            _switch(questions[currentIndex], questions[currentIndex - 1]);
+            currentIndex--;
+        }
+        console.log(currentIndex);
+    });
+})
 // console.log(questions, nextBtn, prevBtn);
