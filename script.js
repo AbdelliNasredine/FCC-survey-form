49d50230bcdac511747c55b9908d74e0201824f2
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-08-21 02:48:54 +0100

    adding input validation using Validation API

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
  *  author: Abdelli Nasredine
  *  description: 
  */
+const survey_form = document.querySelector('form');
 const questions = document.querySelectorAll('.question-container');
 const nextBtn = document.getElementById('next')
 const prevBtn = document.getElementById('previous');
@@ -11,22 +12,26 @@ const isFirst = () => currentIndex == 0;
 const isLast = () => currentIndex === lastIndex;
 const isNext = () => questions[currentIndex + 1] != undefined;
 const isPrev = () => questions[currentIndex - 1] != undefined;
-const _switch = (current, next) => {
-    current.classList.remove("active");
-    next.classList.add("active");
+const _switch = (current, next, fn) => {
+    if (fn()) {
+        current.classList.remove("active");
+        next.classList.add("active");
+    }
 }
-const hideBtns = () => {
+const hideBtns = (fn) => {
     nextBtn.classList.add("not-visible");
     prevBtn.classList.add("not-visible");
 }
 
 window.addEventListener('load', () => {
     nextBtn.addEventListener("click", (event) => {
-        if (isNext()) {
-            _switch(questions[currentIndex], questions[currentIndex + 1]);
+        if (!survey_form.checkValidity()) {
+            survey_form.reportValidity();
+        }else{
+            _switch(questions[currentIndex], questions[currentIndex + 1] , isNext);
             currentIndex++;
         }
-        if(isLast()) hideBtns();
+        if (isLast()) hideBtns();
     });
     prevBtn.addEventListener("click", () => {
         if (isPrev()) {
