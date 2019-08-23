c873dd8b64567985c7fc88ed5ff3d9a5b4e1f7e5
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-08-23 01:47:21 +0100

    fixed some typo

--- static/script.js
+++ static/script.js
@@ -2,39 +2,47 @@
  *  author: Abdelli Nasredine
  *  description: Simple Survy form
  */
-const survey_form = document.querySelector('form');
-const questions = document.querySelectorAll('.question-container');
+const survey_form = document.querySelector('form')
+const questions = document.querySelectorAll('.question-container')
 const nextBtn = document.getElementById('next')
-const prevBtn = document.getElementById('previous');
-const lastIndex = questions.length - 1;
-let currentIndex = 0;
-const isFirst = () => currentIndex == 0;
-const isLast = () => currentIndex === lastIndex;
-const isNext = () => questions[currentIndex + 1] != undefined;
-const isPrev = () => questions[currentIndex - 1] != undefined;
-const _switch = (current, next, fn , reverse = false ) => {
+const prevBtn = document.getElementById('previous')
+const lastIndex = questions.length - 1
+let currentIndex = 0
+const isFirst = () => currentIndex == 0
+const isLast = () => currentIndex === lastIndex
+const isNext = () => questions[currentIndex + 1] != undefined
+const isPrev = () => questions[currentIndex - 1] != undefined
+const _switch = (current, next, fn, reverse = false) => {
     let tmp = reverse ? -1 : 1
     if (fn()) {
-        current.classList.remove("active");
-        next.classList.add("active");
+        current.classList.remove("active")
+        next.classList.add("active")
+        if(!isLast()) show(prevBtn)
         currentIndex += tmp;
     }
 }
-const hideBtns = () => {
-    nextBtn.classList.add("not-visible");
-    prevBtn.classList.add("not-visible");
+const hide = (...btns) => {
+    console.log(btns);
+    btns.forEach((btn) => btn.classList.add("not-visible"))
+}
+const show = (btn) => {
+    btn.classList.remove("not-visible")
 }
 
 window.addEventListener('load', () => {
+
+    // TODO : fix form submit on ENTER key (default form behaviour)
+
     nextBtn.addEventListener("click", (event) => {
+        // input validation using HTML5 validation API
         if (!survey_form.checkValidity()) {
             survey_form.reportValidity();
-        }else{
-            _switch(questions[currentIndex], questions[currentIndex + 1] , isNext);
+            return
         }
-        if (isLast()) hideBtns();
+        _switch(questions[currentIndex], questions[currentIndex + 1], isNext);
+        if (isLast()) hide(nextBtn , prevBtn);
     });
     prevBtn.addEventListener("click", () => {
-            _switch(questions[currentIndex], questions[currentIndex - 1] , isPrev , true);
+        _switch(questions[currentIndex], questions[currentIndex - 1], isPrev, true);
     });
 })
